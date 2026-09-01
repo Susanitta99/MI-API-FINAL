f6dafac3320718ed68ef1d6ce66b3b00f0d2c42a
Susanitta99 <suziyuzekchieva99@gmail.com>
2026-09-01 12:04:48 +0200

    Actualizar mensaje de bienvenida

--- api/v1/hello.js
+++ api/v1/hello.js
@@ -1,5 +1,5 @@
 // Endpoint que devuelve un mensaje de bienvenida en formato JSON.
 module.exports = (req, res) => {
   // Se envía el mensaje "Hola Mundo" como respuesta.
-  res.json({ mensaje: "Hola Mundo" });
+  res.json({ mensaje: "Hola Mundo Bonito" });
 };
