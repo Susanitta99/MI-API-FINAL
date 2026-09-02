852244897bb6a74c1c79dcc14e0e83812deb2dd4
Susanitta99 <suziyuzekchieva99@gmail.com>
2026-09-02 11:45:32 +0200

    Agregar documentacion Swagger

---
+++ api/v1/swagger.js
@@ -0,0 +1,20 @@
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+const swaggerDocument = YAML.load("./api/v1/swagger.yaml");
+
+module.exports = (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Documentación de MI API FINAL</title>
+      </head>
+      <body>
+        <h1>Documentación de MI API FINAL</h1>
+        <p>La documentación Swagger está configurada correctamente.</p>
+        <pre>${JSON.stringify(swaggerDocument, null, 2)}</pre>
+      </body>
+    </html>
+  `);
+};
